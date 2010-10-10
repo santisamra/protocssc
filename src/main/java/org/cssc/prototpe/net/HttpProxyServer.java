8b878fd5b425cefcc2f481360527b1446c689aa1
mcolotto <mcolotto@users.noreply.github.com>
2010-10-10 04:22:18 +0000

    No wanda nara

--- src/main/java/org/cssc/prototpe/net/HttpProxyServer.java
+++ src/main/java/org/cssc/prototpe/net/HttpProxyServer.java
@@ -1,19 +1,20 @@
 package org.cssc.prototpe.net;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class HttpProxyServer {
 
-	private ServerSocket serverSocket;
+	private ServerSocketChannel serverSocket;
 	private Thread listeningThread;
 	private Thread processingThread;
-	private Deque<Socket> clientConnections;
+	private Deque<SocketChannel> clientConnections;
 
 	private class ClientConnectionAccepter implements Runnable {
 		@Override
@@ -21,8 +22,9 @@ public class HttpProxyServer {
 			boolean finished = false;
 			while(!finished) {
 				try {
-					Socket s = serverSocket.accept();
-					System.out.println("ConnectionAccepter - Accepted connection: " + s.getRemoteSocketAddress());
+					SocketChannel s = serverSocket.accept();
+					System.out.println("ConnectionAccepter - Accepted connection: " + s.socket().getRemoteSocketAddress());
+					s.configureBlocking(false);
 					synchronized(clientConnections) {
 						clientConnections.addFirst(s);
 					}
@@ -35,34 +37,50 @@ public class HttpProxyServer {
 
 	private class SocketProcessor implements Runnable {
 
-		private static final int INPUT_BUFFER_SIZE = 1048576; //1MB
+		private static final int INPUT_BUFFER_SIZE = 1024; //1KB
 
 		@Override
 		public void run() {
 			boolean finished = false;
-			byte[] buffer = new byte[INPUT_BUFFER_SIZE];
+//			byte[] buffer = new byte[INPUT_BUFFER_SIZE];
+			ByteBuffer buffer = ByteBuffer.allocateDirect(INPUT_BUFFER_SIZE);
 			while(!finished) {
 				// FIXME: Major performance defect
-				Socket s;
+				SocketChannel s;
 				synchronized(clientConnections) {
 					s = clientConnections.poll();
 				}
 				if(s != null) {
 					try {
-						if(!s.isClosed()) {
-							InputStream inputStream = s.getInputStream();
-							int availableBytes = inputStream.available(); 
+						if(s.isConnected()) {
+//							InputStream inputStream = s.getInputStream();
+//							OutputStream outputStream = s.getOutputStream();
+//							int availableBytes = inputStream.available();
+							
+//							if(availableBytes > 0) {
+//								inputStream.read(buffer, 0, 1000);
+//								System.out.println("SocketProcessor - Received from " + s.getRemoteSocketAddress() + ": ");
+//								print(buffer);
+//								outputStream.write(buffer, 0, availableBytes);
+//								outputStream.flush();
+//							} else {
+//								outputStream.write(0);
+//							}
+							
+//							System.out.println(s.isBlocking());
+							long availableBytes = s.read(buffer);
 							if(availableBytes > 0) {
-								OutputStream outputStream = s.getOutputStream();
-								inputStream.read(buffer, 0, availableBytes);
-								System.out.println("SocketProcessor - Received from " + s.getRemoteSocketAddress() + ": " + buffer.toString());
-								print(buffer);
-								outputStream.write(buffer, 0, availableBytes);
-								outputStream.flush();
+								System.out.println("SocketProcessor - Received: " + availableBytes);
+								System.out.println("Sending: " + buffer.remaining());
+								s.write(buffer);
 							}
+							
 							synchronized(clientConnections){
 								clientConnections.addLast(s);
 							}
+						} else {
+							System.out.println("Connection closed");
+							s.close();
 						}
 					} catch(IOException e) {
 						try {
@@ -93,11 +111,12 @@ public class HttpProxyServer {
 
 	public HttpProxyServer(int port) {
 		try {
-			this.serverSocket = new ServerSocket(port);
+			this.serverSocket = ServerSocketChannel.open();
+			serverSocket.socket().bind(new InetSocketAddress(InetAddress.getLocalHost(), 8080));
 		} catch (IOException e) {
 			throw new FatalException(e);
 		}
-		this.clientConnections = new LinkedList<Socket>();
+		this.clientConnections = new LinkedList<SocketChannel>();
 		listeningThread = new Thread(new ClientConnectionAccepter());
 		listeningThread.start();
 		processingThread = new Thread(new SocketProcessor());
