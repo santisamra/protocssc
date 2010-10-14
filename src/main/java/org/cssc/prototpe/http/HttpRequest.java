086f64b8ba151eef7baf0d6d43315e38a4c4ba91
Santiago José Samra <santisamra@users.noreply.github.com>
2010-10-14 05:46:06 +0000

    Bueno, ahí está el torpe intento. Falta descomentar una línea que haría que en teoría funcione. Habría que ver que pasa con el tema que estamos pasando por un string antes de obtener los bytes....

--- src/main/java/org/cssc/prototpe/http/HttpRequest.java
+++ src/main/java/org/cssc/prototpe/http/HttpRequest.java
@@ -1,5 +1,7 @@
 package org.cssc.prototpe.http;
 
+import java.util.Map.Entry;
+
 public class HttpRequest extends HttpPacket {
 	
 	private String path;
@@ -17,5 +19,28 @@ public class HttpRequest extends HttpPacket {
 	
 	public HttpMethod getMethod() {
 		return method;
+	}
+	
+	//TODO: Determine if this is correct.. I don't like it AT ALL
+	public boolean hasAbsolutePath(){
+		
+		if( getPath().startsWith("/") ){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(this.method.toString() + " " + this.getPath() + " HTTP/" + this.getVersion() + "\n");
+		
+		for(Entry<String,String> e: this.getHeader().getContentMap().entrySet() ){
+			buffer.append(e.getKey() + ": " + e.getValue() + "\n");
+		}
+		
+		buffer.append("\n");
+		return buffer.toString();
 	}
 }
