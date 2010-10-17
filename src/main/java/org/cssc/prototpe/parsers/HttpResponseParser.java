b0a914f656042ef4fc748c827791e762c3a3db88
jeze276 <jscaruli@users.noreply.github.com>
2010-10-17 23:02:12 +0000

    Arreglando la excepción que se tiraba cuando no había contenido.

--- src/main/java/org/cssc/prototpe/parsers/HttpResponseParser.java
+++ src/main/java/org/cssc/prototpe/parsers/HttpResponseParser.java
@@ -31,7 +31,7 @@ public class HttpResponseParser {
 				readInt = inputStream.read();
 			}
 			char readChar = (char)readInt;
-			
+
 			if(readChar == '\r') {
 				if(firstCrRead && firstLfRead) {
 					secondCrRead = true;
@@ -53,7 +53,7 @@ public class HttpResponseParser {
 				secondCrRead = false;
 				secondLfRead = false;
 			}
-			
+
 			buffer.append(readChar);
 		}
 
@@ -65,95 +65,87 @@ public class HttpResponseParser {
 
 		String transferCoding = parsedResponse.getHeader().getField("transfer-encoding");
 
+		byte[] content = new byte[0];
+
 		if(transferCoding == null) {
 			String contentLengthString = parsedResponse.getHeader().getField("content-length");
 
-			if(contentLengthString == null) {
-				throw new InvalidPacketException("The packet has not a transfer encoding nor a content length.");
+			if(contentLengthString != null) {
+
+				int contentLength;
+
+				try {
+					contentLength = Integer.valueOf(contentLengthString);
+				} catch(NumberFormatException e) {
+					throw new InvalidPacketException("Invalid content length.");
+				}
+
+				content = new byte[contentLength];
+
+				int offset = 0;
+				while(offset < contentLength) {
+					offset += inputStream.read(content, offset, contentLength - offset);
+				}
 			}
-
-			int contentLength;
-
-			try {
-				contentLength = Integer.valueOf(contentLengthString);
-			} catch(NumberFormatException e) {
-				throw new InvalidPacketException("Invalid content length.");
-			}
-
-			byte[] content = new byte[contentLength];
-
-			int offset = 0;
-			while(offset < contentLength) {
-				offset += inputStream.read(content, offset, contentLength - offset);
-			}
-
-			return new HttpResponse(
-					parsedResponse.getVersion(),
-					parsedResponse.getHeader(),
-					parsedResponse.getStatusCode(),
-					parsedResponse.getReasonPhrase(),
-					content);
 
 		} else if(transferCoding.toLowerCase().equals("chunked")){
 			/* I read the chunked-bodies. */
 			int contentLength = 0;
-			byte[] content = new byte[contentLength];
-			
+			content = new byte[contentLength];
+
 			/* I read the chunk size. */
 			int chunkSize = readChunkSize();
-			
+
 			while(chunkSize != 0) {
 				byte[] currentChunkData = new byte[chunkSize];
 				int offset = 0;
 				while(offset < chunkSize) {
 					offset += inputStream.read(currentChunkData, offset, chunkSize - offset);
 				}
-				
+
 				int cr = inputStream.read();
 				int lf = inputStream.read();
-//				System.out.println(cr);
-//				System.out.println(lf);
+				//				System.out.println(cr);
+				//				System.out.println(lf);
 				if(cr != 13 || lf != 10) {
 					throw new InvalidPacketException("Invalid chunked data.");
 				}
-				
+
 				/* I append the arrays. */
 				byte[] temp = new byte[contentLength + chunkSize];
 				System.arraycopy(content, 0, temp, 0, contentLength);
 				System.arraycopy(currentChunkData, 0, temp, contentLength, chunkSize);
 				content = temp;
-				
+
 				contentLength += chunkSize;
 				chunkSize = readChunkSize();
 			}
-			
+
 			/* The conent is not chunked now. */
 			parsedResponse.getHeader().removeField("transfer-encoding");
 			parsedResponse.getHeader().setField("content-length", Integer.toString(contentLength));
-			
-			return new HttpResponse(
-					parsedResponse.getVersion(),
-					parsedResponse.getHeader(),
-					parsedResponse.getStatusCode(),
-					parsedResponse.getReasonPhrase(),
-					content);
 		}
-		
-		return null;
+
+		return new HttpResponse(
+				parsedResponse.getVersion(),
+				parsedResponse.getHeader(),
+				parsedResponse.getStatusCode(),
+				parsedResponse.getReasonPhrase(),
+				content);
 	}
-	
+
 	private int readChunkSize() throws IOException {
 		boolean crRead = false;
 		boolean lfRead = false;
 		StringBuffer buffer = new StringBuffer();
-		
+
 		while(!(crRead && lfRead)) {
 			int readInt = -1;
 			while(readInt == -1) {
 				readInt = inputStream.read();
 			}
 			char readChar = (char)readInt;
-			
+
 			if(readChar == '\r') {
 				crRead = true;
 			} else if(readChar == '\n') {
@@ -167,15 +159,15 @@ public class HttpResponseParser {
 				crRead = false;
 				lfRead = false;
 			}
-			
+
 			buffer.append(readChar);
 		}
-		
+
 		try {
 			return Integer.parseInt(buffer.toString().trim(), 16);
 		} catch(NumberFormatException e) {
 			throw new InvalidPacketException("Chunk size must be hexadecimal.");
 		}
 	}
-	
+
 }
