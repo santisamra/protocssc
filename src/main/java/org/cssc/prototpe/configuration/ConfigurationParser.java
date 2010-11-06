c7fbed60596494c3a291bc30b23353a71c2f9eb7
marucura <marucura@0b81a549-61ef-0b0b-f220-f3ce842b6f16>
2010-11-06 23:53:42 +0000

    terminado xml

--- src/main/java/org/cssc/prototpe/configuration/ConfigurationParser.java
+++ src/main/java/org/cssc/prototpe/configuration/ConfigurationParser.java
@@ -1,8 +1,10 @@
 package org.cssc.prototpe.configuration;
 
 import java.net.InetAddress;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,9 +19,19 @@ import org.w3c.dom.NodeList;
 public class ConfigurationParser {
 	
 	private List<ApplicationFilter> filters;
+	private int threadPoolSize;
+	private int maxPersistantServerConnections;
+	private int maxPersistantServerConnectionsPerServer;
+	private String loggingFileName;
+	private InetAddress chainingProxyAddress;
+	private int chainingProxyPort;
+	private int clientKeepAliveTimeout;
+	private int serverConnectionPersistentTimeout;
+	private Map<String, String> adminUsers;
 	
 	public ConfigurationParser(String xmlPath) {
 		filters = new LinkedList<ApplicationFilter>();
+		adminUsers = new HashMap<String, String>();
 		
 	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		FilterCondition condition = null;
@@ -71,23 +83,19 @@ public class ConfigurationParser {
 				    				    					 }
 				    				    				 }
 				    				    			 }
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("browser")){
+				    				    		}else if(currNode.getNodeName().equals("browser")){
 				    				    			browser=currNode.getChildNodes().item(0).getTextContent();
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("OS")){
+				    				    		}else if(currNode.getNodeName().equals("OS")){
 				    				    			oS=currNode.getChildNodes().item(0).getTextContent();
 				    				    		}
 				    				    	}
-				    					}
-				    					else if(filterConf.item(conf).getNodeName().equals("actions")){
+				    					}else if(filterConf.item(conf).getNodeName().equals("actions")){
 				    						NodeList actionsNodeList = filterConf.item(conf).getChildNodes();
 				    				    	for(int i=0; i<actionsNodeList.getLength(); i++){ //por cada config
 				    				    		Node currNode = actionsNodeList.item(i);
 				    				    		if(currNode.getNodeName().equals("block-all-accesses")){
 				    				    			blockAllAccesses=currNode.getChildNodes().item(0).getTextContent().equalsIgnoreCase("true")?true:false;
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("blocked-IPs")){
+				    				    		}else if(currNode.getNodeName().equals("blocked-IPs")){
 				    				    			blockedIPs = new LinkedList<InetAddress>();
 				    				    			 NodeList ips = currNode.getChildNodes();
 				    				    			 for(int j=0; j<ips.getLength(); j++){
@@ -100,8 +108,7 @@ public class ConfigurationParser {
 				    				    					 }
 				    				    				 }
 				    				    			 }
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("blocked-URIs")){
+				    				    		}else if(currNode.getNodeName().equals("blocked-URIs")){
 				    				    			blockedURIs = new LinkedList<String>();
 				    				    			 NodeList uris = currNode.getChildNodes();
 				    				    			 for(int j=0; j<uris.getLength(); j++){
@@ -109,8 +116,7 @@ public class ConfigurationParser {
 				    			    						 blockedURIs.add(uris.item(j).getChildNodes().item(0).getTextContent());
 				    				    				 }
 				    				    			 }
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("blocked-MediaTypes")){
+				    				    		}else if(currNode.getNodeName().equals("blocked-MediaTypes")){
 				    				    			blockedMediaTypes = new LinkedList<String>();
 				    				    			 NodeList mediaTypes = currNode.getChildNodes();
 				    				    			 for(int j=0; j<mediaTypes.getLength(); j++){
@@ -118,8 +124,7 @@ public class ConfigurationParser {
 				    				    					 blockedMediaTypes.add(mediaTypes.item(j).getChildNodes().item(0).getTextContent());
 				    				    				 }
 				    				    			 }
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("max-content-length")){
+				    				    		}else if(currNode.getNodeName().equals("max-content-length")){
 				    				    			try {
 				    				    				maxContentLength = Integer.parseInt(currNode.getChildNodes().item(0).getTextContent());
 				    				    				
@@ -129,8 +134,7 @@ public class ConfigurationParser {
 				    				    			}catch (NumberFormatException e) {
 				    				    				throw new ConfigurationParserException("Invalid max content length.");
 				    								}
-				    				    		}
-				    				    		else if(currNode.getNodeName().equals("transform")) {
+				    				    		}else if(currNode.getNodeName().equals("transform")) {	
 				    				    			 NodeList transforms = currNode.getChildNodes();
 				    				    			 
 				    				    			 for(int j = 0; j < transforms.getLength(); j++) {
@@ -165,11 +169,75 @@ public class ConfigurationParser {
 				    				filters.add(filter);
 			    	    		}
 			    			}
+			    		}else if(configs.item(l).getNodeName().equals("threadPoolSize")){
+			    			try{
+			    				threadPoolSize=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid thread pool size.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("maxPersistantServerConnections")){
+			    			try{
+			    				maxPersistantServerConnections=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid persistant server connections.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("maxPersistantServerConnectionsPerServer")){
+			    			try{
+			    				maxPersistantServerConnectionsPerServer=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid persistant server connections per server.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("loggingFileName")){
+		    				loggingFileName=configs.item(l).getChildNodes().item(0).getTextContent();
+			    		}else if(configs.item(l).getNodeName().equals("chainingProxyAddress")){
+			    			try {
+								IP ip = new IP(configs.item(l).getChildNodes().item(0).getTextContent());
+								chainingProxyAddress=ip.getInetAddress();
+	    					 } catch (Exception e) {
+	    						 throw new ConfigurationParserException("Invalid chaining proxy address.");
+	    					 }
+			    		}else if(configs.item(l).getNodeName().equals("chainingProxyPort")){
+			    			try{
+			    				chainingProxyPort=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid chaining proxy port.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("clientKeepAliveTimeout")){
+			    			try{
+			    				clientKeepAliveTimeout=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid client keep-alive timeout.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("serverConnectionPersistentTimeout")){
+			    			try{
+			    				serverConnectionPersistentTimeout=Integer.parseInt(configs.item(l).getChildNodes().item(0).getTextContent());
+			    			}catch(NumberFormatException e){
+			    				throw new ConfigurationParserException("Invalid server connection persistent timeout.");
+			    			}
+			    		}else if(configs.item(l).getNodeName().equals("admin-users")){
+			    			NodeList users = configs.item(l).getChildNodes();
+			    			for(int h=0; h<users.getLength(); h++){
+			    				if(users.item(h).getNodeName().equals("user")){
+			    					NodeList combo = users.item(h).getChildNodes();
+			    					String name=null, pass=null;
+			    					for(int i=0; i<combo.getLength(); i++){
+			    						if(combo.item(i).getNodeName().equals("name")){
+			    							name = combo.item(i).getChildNodes().item(0).getTextContent();
+			    						}else if(combo.item(i).getNodeName().equals("pass")){
+			    							pass = combo.item(i).getChildNodes().item(0).getTextContent();
+			    						}
+			    					}
+			    					if(name!=null && pass!=null){
+			    						adminUsers.put(name, pass);
+			    					}else{
+			    						throw new ConfigurationParserException("Invalid name-password combination.");
+			    					}
+			    				}
+			    			}
 			    		}
 			    	}
 		    	}
 	    	}
-	    	
 	    } catch (Exception e) {
 	    	e.printStackTrace();
 	    	throw new ConfigurationParserException("Could not parse the configuration file.");
@@ -178,5 +246,57 @@ public class ConfigurationParser {
 	
 	public List<ApplicationFilter> getFilters() {
 		return filters;
+	}
+
+	public int getThreadPoolSize() {
+		return threadPoolSize;
+	}
+
+	public int getMaxPersistantServerConnections() {
+		return maxPersistantServerConnections;
+	}
+
+	public int getMaxPersistantServerConnectionsPerServer() {
+		return maxPersistantServerConnectionsPerServer;
+	}
+
+	public String getLoggingFileName() {
+		return loggingFileName;
+	}
+
+	public InetAddress getChainingProxyAddress() {
+		return chainingProxyAddress;
+	}
+
+	public int getChainingProxyPort() {
+		return chainingProxyPort;
+	}
+
+	public int getClientKeepAliveTimeout() {
+		return clientKeepAliveTimeout;
+	}
+
+	public int getServerConnectionPersistentTimeout() {
+		return serverConnectionPersistentTimeout;
+	}
+
+	public Map<String, String> getAdminUsers() {
+		return adminUsers;
+	}
+
+	@Override
+	public String toString() {
+		return "ConfigurationParser [filters=" + filters + ", threadPoolSize="
+				+ threadPoolSize + ", maxPersistantServerConnections="
+				+ maxPersistantServerConnections
+				+ ", maxPersistantServerConnectionsPerServer="
+				+ maxPersistantServerConnectionsPerServer
+				+ ", loggingFileName=" + loggingFileName
+				+ ", chainingProxyAddress=" + chainingProxyAddress
+				+ ", chainingProxyPort=" + chainingProxyPort
+				+ ", clientKeepAliveTimeout=" + clientKeepAliveTimeout
+				+ ", serverConnectionPersistentTimeout="
+				+ serverConnectionPersistentTimeout + ", adminUsers="
+				+ adminUsers + "]";
 	}
 }
